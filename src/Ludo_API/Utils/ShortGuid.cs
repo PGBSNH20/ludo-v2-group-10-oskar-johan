a50fd81782a2ebc858405e19667a6cbc5b60cbec
JoCh <johancz@users.noreply.github.com>
2021-05-13 20:44:36 +0200

    Add a utility class "ShortGuid"
    - Can be used to encode a Guid to an URL friendly string and decode it back to a Guid.

---
+++ src/Ludo_API/Utils/ShortGuid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ludo_API.Utils
+{
+    // Based on: https://stackoverflow.com/a/40917033
+    public static class ShortGuid
+    {
+        public static string CreateShortGuid()
+        {
+            string base64Guid = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            // Replace URL unfriendly characters and remove trailing ==
+            return base64Guid.Replace('+', '-').Replace('/', '_')[..^2];
+        }
+
+        public static Guid FromShortGuid(string @string)
+        {
+            @string = @string.Replace('_', '/').Replace('-', '+');
+            return new Guid(Convert.FromBase64String(@string + "=="));
+        }
+    }
+}
