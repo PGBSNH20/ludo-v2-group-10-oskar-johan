0a6310e4735a150296b2bb461dfa9882b74be898
Oskar Karlsson <tilloskar@outlook.com>
2021-05-19 11:49:17 +0200

    fixed typo

--- src/Ludo_WebApp/Hubs/LudoHub.cs
+++ src/Ludo_WebApp/Hubs/LudoHub.cs
@@ -10,7 +10,7 @@ namespace Ludo_WebApp.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("RecieveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
 }
