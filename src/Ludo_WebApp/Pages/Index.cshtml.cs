624aee13202bb6109770064afa0fb2f3d947db30
JoCh <johancz@users.noreply.github.com>
2021-05-23 13:34:39 +0200

    Add (temp) table of gameboards to index page
    - This required adding an API call to api/Games/ to get all gameboards

--- src/Ludo_WebApp/Pages/Index.cshtml.cs
+++ src/Ludo_WebApp/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ludo_WebApp.Ludo_API;
+using Ludo_WebApp.Models;
+using Ludo_WebApp.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ludo_WebApp.Pages
@@ -12,14 +16,25 @@ namespace Ludo_WebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty(SupportsGet = true)]
+        public List<GameboardDTO> Gameboards { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var restResponse = await Fetch.GetAsync<List<GameboardDTO>>(Fetch.RequestURLs.Games);
 
+            if (restResponse.StatusCode != HttpStatusCode.OK)
+            {
+                ModelState.AddModelError("ApiErrorGetGames", restResponse.Content);
+                // todo: redirect to error page?
+            }
+
+            Gameboards = restResponse.Data;
         }
     }
 }
