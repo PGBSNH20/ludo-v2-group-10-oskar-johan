c623039d21bef9f7ab994654ac0ea25728a6ba00
JoCh <johancz@users.noreply.github.com>
2021-05-23 13:31:25 +0200

    Update Fetch.cs and add LudoData to WebApp
    - Add generic method for sending GET requests.
        - Takes a requestURL (e.g. "/Games/") and an object of query-parameters. This object is converted to a dictionary using TypeDescriptor and PropertyDescriptorCollection and then converted to a query-string.
    - Add method for fetching LudoData from the API.

--- src/Ludo_WebApp/Ludo_API/Fetch.cs
+++ src/Ludo_WebApp/Ludo_API/Fetch.cs
@@ -1,8 +1,12 @@
-﻿using Ludo_WebApp.Models;
+﻿using Ludo_WebApp.Ludo_API.Models;
+using Ludo_WebApp.Models;
+using Ludo_WebApp.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -12,47 +16,20 @@ namespace Ludo_WebApp.Ludo_API
     {
         private const string _baseURL = "https://localhost:44376/api";
 
-        private struct RequestURLs
+        internal struct RequestURLs
         {
-            internal static string NewGame = "/Games/New/";
-            internal static string AddPlayer = "/Games/AddPlayer/";
-            internal static string GetGame = "/Games/";
-            internal static string StartGame = "/Games/StartGame/";
+            internal static string Games = "/Games/";
+            internal static string GamesLudoData = "/Games/LudoData/";
+            internal static string GamesNew = "/Games/New/";
+            internal static string GamesAddPlayer = "/Games/AddPlayer/";
+            internal static string GamesStartGame = "/Games/StartGame/";
         }
-
-        //// Generic method for fetching data from the API (swapi.com)
-        //public static async Task<List<T>> Data<T>(string requestUrl)
-        //{
-        //    try
-        //    {
-        //        var client = new RestClient(_baseURL);
-        //        APIResponse<T> response;
-        //        List<T> result = new();
-
-        //        while (requestUrl != null)
-        //        {
-        //            string resource = requestUrl.Substring(_baseURL.Length);
-        //            var request = new RestRequest(resource, DataFormat.Json);
-        //            response = await client.GetAsync<APIResponse<T>>(request);
-
-        //            result.AddRange(response.Results);
-        //            requestUrl = response.Next;
-        //        }
-
-        //        return result;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e.Message);
-        //        throw;
-        //    }
-        //}
 
         public static async Task<IRestResponse<int>> PostNewGameAsync(NewPlayerDTO newPlayerDTO)
         //public static async Task<IRestResponse<GameboardDTO>> PostNewGameAsync(NewPlayerDTO newPlayerDTO)
         {
             var client = new RestClient(_baseURL);
-            var request = new RestRequest(RequestURLs.NewGame);
+            var request = new RestRequest(RequestURLs.GamesNew, Method.POST);
             request.AddJsonBody(newPlayerDTO);
             //request.OnBeforeDeserialization = r => { r.ContentType = "application/json"; };
 
@@ -75,7 +52,7 @@ namespace Ludo_WebApp.Ludo_API
         public static async Task<IRestResponse<GameboardDTO>> PostAddPlayerAsync(NewPlayerDTO newPlayerDTO)
         {
             var client = new RestClient(_baseURL);
-            var request = new RestRequest(RequestURLs.AddPlayer, Method.POST);
+            var request = new RestRequest(RequestURLs.GamesAddPlayer, Method.POST);
             request.AddJsonBody(newPlayerDTO);
             //request.OnBeforeDeserialization = r => { r.ContentType = "application/json"; };
 
@@ -95,12 +72,59 @@ namespace Ludo_WebApp.Ludo_API
             }
         }
 
-        // api/Games/{id}
-        //internal async static Task<ActionResult<GameboardDTO>> GetGame(int gameId)
-        internal async static Task<IRestResponse<GameboardDTO>> GetGame(int gameId)
+        internal static async Task<IRestResponse<T>> GetAsync<T>(string requestURL, object queryParameters = null)
+        {
+            // The source code for RouteValueDictionary as used by the RedirectToPage class like so: 'RedirectToPage("./Index/", new { id = 5 }),
+            // was used to figured out how to convert an object of keys and values to a dictionary<string, string> of keys and values.
+            var dict = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+
+            if (queryParameters != null)
+            {
+                PropertyDescriptorCollection @params = TypeDescriptor.GetProperties(queryParameters);
+                foreach (PropertyDescriptor param in @params)
+                {
+                    dict.Add(param.Name, param.GetValue(queryParameters));
+                }
+            }
+
+            string queryString = dict?.Count > 0 ? "?" + string.Join("&", dict.Select(param => param.Key + "=" + param.Value)) : "";
+
+            var client = new RestClient(_baseURL);
+            var request = new RestRequest(RequestURLs.Games + queryString, Method.GET);
+
+            try
+            {
+                return await client.ExecuteAsync<T>(request);
+            }
+            catch (Exception)
+            {
+                // log the error?
+                return null;
+            }
+        }
+
+        internal static async Task<IRestResponse<List<GameboardDTO>>> GetGames()
         {
             var client = new RestClient(_baseURL);
-            var request = new RestRequest(RequestURLs.GetGame + gameId, Method.GET);
+            var request = new RestRequest(RequestURLs.Games, Method.GET);
+
+            try
+            {
+                return await client.ExecuteAsync<List<GameboardDTO>>(request);
+            }
+            catch (Exception)
+            {
+                // log the error?
+                return null;
+            }
+        }
+
+        // api/Games/{id}
+        //internal static async Task<ActionResult<GameboardDTO>> GetGame(int gameId)
+        internal static async Task<IRestResponse<GameboardDTO>> GetGame(int gameId)
+        {
+            var client = new RestClient(_baseURL);
+            var request = new RestRequest(RequestURLs.Games + gameId, Method.GET);
 
             try // todo: fix getting and returning the error msg from throw in SetTrack
             {
@@ -116,11 +140,27 @@ namespace Ludo_WebApp.Ludo_API
             }
         }
 
-        //internal static Task PostStartGameAsync(NewPlayerDTO newPlayer)
-        internal async static Task<IRestResponse<GameboardDTO>> StartGameAsync(int gameId)
+        internal static async Task<IRestResponse<LudoData>> GetLudoData()
         {
             var client = new RestClient(_baseURL);
-            var request = new RestRequest(RequestURLs.StartGame, Method.POST);
+            var request = new RestRequest(RequestURLs.GamesLudoData, Method.GET);
+
+            try
+            {
+                return await client.ExecuteAsync<LudoData>(request);
+            }
+            catch (Exception)
+            {
+                // log the error?
+                return null;
+            }
+        }
+
+        //internal static Task PostStartGameAsync(NewPlayerDTO newPlayer)
+        internal static async Task<IRestResponse<GameboardDTO>> StartGameAsync(int gameId)
+        {
+            var client = new RestClient(_baseURL);
+            var request = new RestRequest(RequestURLs.GamesStartGame, Method.POST);
             request.AddJsonBody(gameId);
 
             try // todo: fix getting and returning the error msg from throw in SetTrack
@@ -137,7 +177,7 @@ namespace Ludo_WebApp.Ludo_API
             }
         }
 
-        //internal async static Task<IRestResponse<T> PostAsync<T1, T2>(T1 object) {
+        //internal static async Task<IRestResponse<T> PostAsync<T1, T2>(T1 object) {
         //    }
     }
 }
---
+++ src/Ludo_WebApp/Ludo_API/Models/LudoData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Ludo_WebApp.Ludo_API.Models
+{
+    public class ColorTrackData
+    {
+        public List<int> TrackIndices { get; set; }
+        public int StartIndex { get; set; }
+        public int GoalIndex { get; set; }
+        public string ColorHex { get; set; }
+        public char ColorMapKey { get; set; }
+
+        public ColorTrackData()
+        {
+        }
+    }
+
+    public class LudoData
+    {
+        public int GameboardRowCount { get; set; }
+        public int GameboardColumnCount { get; set; }
+        public List<List<int>> GameboardMapIndices { get; set; }
+        //public List<string> GameboardMapColors { get; set; }
+        public List<List<char>> GameboardMapColors { get; set; }
+        public Dictionary<char, string> ColorKeyMap { get; set; }
+        public Dictionary<string, ColorTrackData> ColorTracks { get; set; }
+
+
+        public LudoData()
+        {
+        }
+    }
+}
