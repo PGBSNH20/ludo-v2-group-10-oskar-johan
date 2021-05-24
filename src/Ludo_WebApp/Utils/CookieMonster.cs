41490e1bcc3ab5c25b40ee9b8495c644933ad99d
Oskar Karlsson <tilloskar@outlook.com>
2021-05-24 15:26:05 +0200

    Added CookeMonster class

---
+++ src/Ludo_WebApp/Utils/CookieMonster.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ludo_WebApp.Utils
+{
+    public static class CookieMonster
+    {
+        public static void SetCookie(IResponseCookies cookies, string key, string value, int? expireTime = null)
+        {
+            CookieOptions option = new CookieOptions();
+
+            if (expireTime.HasValue)
+            {
+                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            }
+            else
+            {
+                option.Expires = DateTime.Now.AddYears(1);
+            }
+
+            cookies.Append(key, value, option);
+        }
+    }
+}
