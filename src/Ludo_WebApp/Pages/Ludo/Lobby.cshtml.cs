5f6f8c980dd64b222225e1a74139f1e3c151b5c0
JoCh <johancz@users.noreply.github.com>
2021-05-27 12:59:02 +0200

    Add Pages/Ludo/Lobby to WebApp
    This is a new pages for creating and joining games.

---
+++ src/Ludo_WebApp/Pages/Ludo/Lobby.cshtml.cs
@@ -0,0 +1,105 @@
+using Ludo_WebApp.Ludo_API;
+using Ludo_WebApp.Ludo_API.Models;
+using Ludo_WebApp.Models.DTO;
+using Ludo_WebApp.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Ludo_WebApp.Pages.Ludo
+{
+    public class LobbyModel : PageModel
+    {
+        /* --- Bound Properties ----------------------------- */
+
+        [BindProperty]
+        public NewPlayerDTO NewPlayer { get; set; }
+
+
+        /* --- Properties ----------------------------------- */
+
+        public LudoData LudoData { get; set; }
+        public GameboardDTO Gameboard { get; set; }
+
+
+        /* --- Actions -------------------------------------- */
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id != null)
+            {
+                /* --- Get the Gameboard with the {id} ------------- */
+
+                //var restResponseGame = await Fetch.GetAsync<GameboardDTO>(Fetch.RequestURLs.Games, new { id = id.Value });
+                var restResponseGame = await Fetch.GetAsync<GameboardDTO>(Fetch.RequestURLs.Games,  id.Value);
+
+                if (restResponseGame.StatusCode != HttpStatusCode.OK)
+                {
+                    ModelState.AddModelError("Error.Lobby.OnGet.GetGameboardDTO.Debug", restResponseGame.ErrorMessage);
+                    ModelState.AddModelError("Error.Lobby.OnGet.GetGameboardDTO", restResponseGame.Content);
+                    return Page();
+                }
+
+                Gameboard = restResponseGame.Data;
+            }
+
+            /* --- Get Ludodata -------------------------------- */
+
+            // Check if this is already stored in a cookie/local storage
+            // Read cookie on server and automatically send if not set?
+            var restResponseLudoData = await Fetch.GetAsync<LudoData>(Fetch.RequestURLs.GamesLudoData);
+
+            if (restResponseLudoData.StatusCode != HttpStatusCode.OK)
+            {
+                ModelState.AddModelError("Error.Lobby.OnGet.GetLudoData.Debug", restResponseLudoData.ErrorMessage);
+                ModelState.AddModelError("Error.Lobby.OnGet.GetLudoData", restResponseLudoData.Content);
+                return Page();
+            }
+
+            LudoData = restResponseLudoData.Data;
+
+            return Page();
+        }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "ModelState invalid");
+                return Page();
+            }
+
+            if (NewPlayer.GameId == null)
+            {
+                // Create a new game with the new player
+                var restResponse = await Fetch.PostAsync<GameboardDTO>(Fetch.RequestURLs.GamesNew, NewPlayer);
+
+                if (restResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    ModelState.AddModelError("Error.Lobby.OnPost.NewGame.Debug", restResponse.ErrorMessage);
+                    ModelState.AddModelError("Error.Lobby.OnPost.NewGame", restResponse.Content);
+                    return Page();
+                }
+
+                return RedirectToPage("./Lobby", new { id = restResponse.Data.ID });
+            }
+            else
+            {
+                // Add the new player to the game
+                var restResponse = await Fetch.PostAsync<NewPlayerDTO>(Fetch.RequestURLs.GamesAddPlayer, NewPlayer);
+
+                if (restResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    ModelState.AddModelError("Error.Lobby.OnPost.AddPlayer.Debug", restResponse.ErrorMessage);
+                    ModelState.AddModelError("Error.Lobby.OnPost.AddPlayer", restResponse.Content);
+                    return Page();
+                }
+
+                return RedirectToPage("./Lobby", new { id = restResponse.Data.ID });
+            }
+        }
+    }
+}
