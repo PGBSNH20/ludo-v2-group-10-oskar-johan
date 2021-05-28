951dca601ad9a0dc9406d0e39ab02cb47d224446
JoCh <johancz@users.noreply.github.com>
2021-05-28 08:39:03 +0200

    Fix errors in LudoData and add ILudoData2 and LudoData2 (wip replacement)

---
+++ src/Ludo_API/Data/ILudoData2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ludo_API.Data
+{
+    public interface ILudoData2
+    {
+        public Dictionary<string, List<int>> Tracks { get; }
+    }
+}
