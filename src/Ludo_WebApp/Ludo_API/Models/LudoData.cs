9f3a2fd64950445ac631841d9be4106b906016ee
JoCh <johancz@users.noreply.github.com>
2021-05-29 22:44:20 +0200

    Fix messages not displaying and styling of messages

--- src/Ludo_WebApp/Ludo_API/Models/LudoData.cs
+++ src/Ludo_WebApp/Ludo_API/Models/LudoData.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 
 namespace Ludo_WebApp.Ludo_API.Models
 {
+    static public class MoveMessagesClass
+    {
+        public const string NoPossibleMoves = "No possible moves";
+        public const string MoveSuccessful = "Move successful";
+        public const string CantPassYourOwn = "You can't pass or stop on a square you occupy";
+        public const string KnockOutOpponent = "You've knocked your opponent's piece(s) out.";
+        public const string PieceEnteredGoal = "You've moved a piece to the goal square.";
+    }
+
     public class ColorTrackData
     {
         public List<int> TrackIndices { get; set; }
